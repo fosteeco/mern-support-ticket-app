77e6ce4a83ccf388aaba715a3db87b1081fcb32a
Christian Foster <fosterchris000@gmail.com>
2022-02-08 13:07:36 -0500

    login and register page are now using global state with redux

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -8,6 +8,20 @@ const initialState = {
   message: "",
 };
 
+/* 'auth/register' could be anything, second argument is an async function */
+// Register new user
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkAPI) => {
+    console.log(user);
+  }
+);
+
+// Login user
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  console.log(user);
+});
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
