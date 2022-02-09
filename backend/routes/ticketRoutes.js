d09382650d53256f1e6460fb298f3ba228d97d36
Christian Foster <fosterchris000@gmail.com>
2022-02-09 13:52:35 -0500

    getTicket, updateTicket, and deleteTicket now implemented. Full CRUD.

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const { createTicket, getTickets } = require("../controllers/ticketController");
+const {
+  createTicket,
+  getTickets,
+  updateTicket,
+  deleteTicket,
+  getTicket,
+} = require("../controllers/ticketController");
 
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").get(protect, getTickets).post(protect, createTicket);
+
+router
+  .route("/:id")
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
 
 module.exports = router;
