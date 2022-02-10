7d181c2152c6789ec4a3be0101e6a3b1cb42b344
Christian Foster <fosterchris000@gmail.com>
2022-02-10 14:02:36 -0500

    Created notes route

---
+++ backend/models/noteModel.js
@@ -0,0 +1,34 @@
+/* Our schema, all the fields we want for our tickets */
+const mongoose = require("mongoose");
+
+const noteSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Ticket",
+    },
+    text: {
+      type: String,
+      required: [true, "Please add some text"],
+    },
+    isStaff: {
+      type: Boolean,
+      default: false,
+    },
+    staffId: {
+      type: String,
+    },
+  },
+  {
+    /* will have created at and updated at  */
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Note", noteSchema);
